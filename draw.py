afc467d03c166f6c36f692c19dfb5d597bf1162f
chb2ab <chb2ab@virginia.edu>
2015-10-24 14:13:09 -0400

    'r' for random points

--- draw.py
+++ draw.py
@@ -8,27 +8,35 @@ from matplotlib.lines import Line2D
 from matplotlib.artist import Artist
 from matplotlib.mlab import dist_point_to_segment
 import matplotlib.pyplot as plt
+from numpy.random import rand
 
 
 class customPointPlacer:
-    """
-    Key-bindings
-      'i' insert a vertex at the current position of the cursor.
-    """
-    def __init__(self, fig, ax, xs, ys):
-        self.fig = fig;
-        self.ax = ax;
-        self.points, = self.ax.plot(xs, ys, 'o')
-        self.xs = xs;
-        self.ys = ys;
+	"""
+	Key-bindings
+	  'i' insert a vertex at the current position of the cursor.
+	"""
+	def __init__(self, fig, ax, xs, ys):
+		self.fig = fig;
+		self.ax = ax;
+		self.points, = self.ax.plot(xs, ys, 'o')
+		self.xs = xs;
+		self.ys = ys;
 
-    def key_press_callback(self, event):
-        'whenever a key is pressed'
-        if not event.inaxes: return
-        elif event.key=='i':
-            self.xs.append(event.xdata);
-            self.ys.append(event.ydata);
-            self.points.set_xdata(self.xs);
-            self.points.set_ydata(self.ys);
-            self.fig.canvas.draw();
-            self.fig.canvas.flush_events();
+	def key_press_callback(self, event):
+		'whenever a key is pressed'
+		if not event.inaxes: return
+		elif event.key=='i':
+			self.xs.append(event.xdata);
+			self.ys.append(event.ydata);
+			self.points.set_xdata(self.xs);
+			self.points.set_ydata(self.ys);
+			self.fig.canvas.draw();
+			self.fig.canvas.flush_events();
+		elif event.key=='r':
+			self.xs.append(event.xdata+rand(1)-0.5);
+			self.ys.append(event.ydata+rand(1)-0.5);
+			self.points.set_xdata(self.xs);
+			self.points.set_ydata(self.ys);
+			self.fig.canvas.draw();
+			self.fig.canvas.flush_events();
